4c7dc87311823bf96893124e85ff795b6482bdf2
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-12-09 15:30:06 -0300

    create-navbar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* Open the navbar links menu on mobile */
+function navbarToggle() {
+    let navbartoggle = document.querySelector('.navbar-toggle');
+
+    if (navbartoggle.style.display === "none") {
+        navbartoggle.style.display = "block";
+    } else {
+        navbartoggle.style.display = "none";
+    }
+}
